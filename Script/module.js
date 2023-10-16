fe5c1a81a4f3b1e27d7bc578728a7334321618fb
Sonsim <141270918+Sonsim@users.noreply.github.com>
2023-10-16 15:01:22 +0200

    html created

--- Script/module.js
+++ Script/module.js
@@ -74,9 +74,10 @@ const model = {
                 age: '30',
                 location: 'Lillestrøm',
                 password: 'abcs23',
+                img: 'imgurl',
                 pets: [
                     {
-                        img: 'img/Easy1.jpg', //default verdi burde være et standard bilde
+                        img: 'img/Easy1.jpg', // default verdi burde være bilde
                         type: 'Husky-Samoyed-mix',
                         name: "Easy",
                         age: '3',
@@ -92,7 +93,7 @@ const model = {
                 pets:[
                     {
                         img: 'imgurl',
-                        type: 'dachshund',
+                        type: 'Dachshund',
                         name: "Frøya",
                         age: '3',
                         favoriteToy: 'Puten hennes/ hånden min',
@@ -103,6 +104,7 @@ const model = {
                 age: "25",
                 location: "Nøtterøy",
                 password: '21345',
+                img: 'imgurl',
                 pets: [
                     {
                         img: 'imgurl',
@@ -110,6 +112,486 @@ const model = {
                         name: "Ludo",
                         age: '3',
                         favoriteToy: 'Hender',
+                    },
+                ],
+            },
+            Emma: {
+                age: '28',
+                location: 'Oslo',
+                password: 'password123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Maine Coon',
+                        name: "Fluffy",
+                        age: '5',
+                        favoriteToy: 'Feather wand',
+                    },
+                ],
+            },
+            Mia: {
+                age: '32',
+                location: 'Bergen',
+                password: 'catlover99',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Labrador Retriever',
+                        name: "Buddy",
+                        age: '2',
+                        favoriteToy: 'Squeaky ball',
+                    },
+                ],
+            },
+            Erik: {
+                age: '40',
+                location: 'Trondheim',
+                password: 'doggy123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Golden Retriever',
+                        name: "Max",
+                        age: '4',
+                        favoriteToy: 'Tennis ball',
+                    },
+                ],
+            },
+            Ingrid: {
+                age: '27',
+                location: 'Stavanger',
+                password: 'pawprint22',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Siamese', 
+                        name: "Luna",
+                        age: '2',
+                        favoriteToy: 'Feather teaser',
+                    },
+                ],
+            },
+            Martin: {
+                age: '35',
+                location: 'Tromsø',
+                password: 'martinpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Beagle', //endre dyr
+                        name: "Charlie",
+                        age: '6',
+                        favoriteToy: 'Plush squirrel',
+                    },
+                ],
+            },
+            Julie: {
+                age: '29',
+                location: 'Drammen',
+                password: 'julie123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Persian', 
+                        name: "Leo",
+                        age: '4',
+                        favoriteToy: 'Feather ball',
+                    },
+                ],
+            },
+            Sara: {
+                age: '33',
+                location: 'Kristiansand',
+                password: 'sara567',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Bulldog', //endre dyr
+                        name: "Rocky",
+                        age: '4',
+                        favoriteToy: 'Squeaky bone',
+                    },
+                ],
+            },
+            Andreas: {
+                age: '31',
+                location: 'Fredrikstad',
+                password: 'andy123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Ragdoll', 
+                        name: "Mittens",
+                        age: '3',
+                        favoriteToy: 'Yarn ball',
+                    },
+                ],
+            },
+            Karoline: {
+                age: '26',
+                location: 'Ålesund',
+                password: 'karolinepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'German Shepherd', // endre dyr
+                        name: "Rex",
+                        age: '5',
+                        favoriteToy: 'Tug toy',
+                    },
+                ],
+            },
+            Thomas: {
+                age: '37',
+                location: 'Drammen',
+                password: 'thomas456',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Tabby',
+                        name: "Whiskers",
+                        age: '4',
+                        favoriteToy: 'Feather wand',
+                    },
+                ],
+            },
+            Kristine: {
+                age: '28',
+                location: 'Bodø',
+                password: 'kristinepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Border Collie', // endre dyr
+                        name: "Riley",
+                        age: '3',
+                        favoriteToy: 'Frisbee',
+                    },
+                ],
+            },
+            Oskar: {
+                age: '24',
+                location: 'Haugesund',
+                password: 'oskarpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Rottweiler', // endre dyr
+                        name: "Bruno",
+                        age: '4',
+                        favoriteToy: 'Kong toy',
+                    },
+                ],
+            },
+            Sandra: {
+                age: '29',
+                location: 'Molde',
+                password: 'sandra89',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Siberian Husky', // endre dyr
+                        name: "Sky",
+                        age: '3',
+                        favoriteToy: 'Rope toy',
+                    },
+                ],
+            },
+            Christian: {
+                age: '34',
+                location: 'Sandefjord',
+                password: 'christianpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Persian',
+                        name: "Fluffy",
+                        age: '4',
+                        favoriteToy: 'Feather teaser',
+                    },
+                ],
+            },
+            Henriette: {
+                age: '27',
+                location: 'Moss',
+                password: 'henriettepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Bengal', // endre
+                        name: "Simba",
+                        age: '2',
+                        favoriteToy: 'Mouse toy',
+                    },
+                ],
+            },
+            Emil: {
+                age: '25',
+                location: 'Halden',
+                password: 'emilpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Cavalier King Charles Spaniel', // endre
+                        name: "Charlie",
+                        age: '3',
+                        favoriteToy: 'Chew toy',
+                    },
+                ],
+            },
+            Martine: {
+                age: '29',
+                location: 'Ås',
+                password: 'martinepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Sphynx', // endre
+                        name: "Nala",
+                        age: '2',
+                        favoriteToy: 'Feather wand',
+                    },
+                ],
+            },
+            AndreasO: {
+                age: '32',
+                location: 'Gjøvik',
+                password: 'andyo123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Labradoodle', // endre
+                        name: "Bella",
+                        age: '3',
+                        favoriteToy: 'Bouncy ball',
+                    },
+                ],
+            },
+            Anette: {
+                age: '27',
+                location: 'Larvik',
+                password: 'anettepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Ragdoll', // endre
+                        name: "Milo",
+                        age: '2',
+                        favoriteToy: 'Feather ball',
+                    },
+                ],
+            },
+            Fredrik: {
+                age: '36',
+                location: 'Kristiansund',
+                password: 'fredrikpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Doberman Pinscher', // endre
+                        name: "Thor",
+                        age: '4',
+                        favoriteToy: 'Tug rope',
+                    },
+                ],
+            },
+            Vilde: {
+                age: '24',
+                location: 'Mandal',
+                password: 'vildepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Abyssinian', // endre
+                        name: "Leo",
+                        age: '2',
+                        favoriteToy: 'Feather teaser',
+                    },
+                ],
+            },
+            Tom: {
+                age: '30',
+                location: 'Drammen',
+                password: 'tom123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Bulldog', // endre
+                        name: "Rocky",
+                        age: '3',
+                        favoriteToy: 'Squeaky bone',
+                    },
+                ],
+            },
+            Maria: {
+                age: '31',
+                location: 'Stavanger',
+                password: 'maria123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Sphynx', // endre
+                        name: "Luna",
+                        age: '2',
+                        favoriteToy: 'Laser pointer',
+                    },
+                ],
+            },
+            Simen: {
+                age: '28',
+                location: 'Tønsberg',
+                password: 'simenpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'German Shepherd', // endre
+                        name: "Rex",
+                        age: '5',
+                        favoriteToy: 'Tug toy',
+                    },
+                ],
+            },
+            Malin: {
+                age: '26',
+                location: 'Kristiansand',
+                password: 'malinpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Persian', // endre
+                        name: "Whiskers",
+                        age: '4',
+                        favoriteToy: 'Feather wand',
+                    },
+                ],
+            },
+            Oliver: {
+                age: '32',
+                location: 'Haugesund',
+                password: 'oliver123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Ragdoll', // endre
+                        name: "Kitty",
+                        age: '3',
+                        favoriteToy: 'Mouse toy',
+                    },
+                ],
+            },
+            Amanda: {
+                age: '29',
+                location: 'Molde',
+                password: 'amanda123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Bengal', // endre
+                        name: "Simba",
+                        age: '2',
+                        favoriteToy: 'Laser pointer',
+                    },
+                ],
+            },
+            Petter: {
+                age: '24',
+                location: 'Lillestrøm',
+                password: 'petterpass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Labrador Retriever', //endre
+                        name: "Bella",
+                        age: '3',
+                        favoriteToy: 'Squeaky ball',
+                    },
+                ],
+            },
+            Louise: {
+                age: '29',
+                location: 'Bergen',
+                password: 'louisepass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Siberian Husky', //endre
+                        name: "Sky",
+                        age: '3',
+                        favoriteToy: 'Frisbee',
+                    },
+                ],
+            },
+            Jonathan: {
+                age: '34',
+                location: 'Stavanger',
+                password: 'jonathan123',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Maine Coon', //endre
+                        name: "Whiskers",
+                        age: '4',
+                        favoriteToy: 'Feather ball',
+                    },
+                ],
+            },
+            Nora: {
+                age: '27',
+                location: 'Tromsø',
+                password: 'norapass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Bulldog', //endre
+                        name: "Rocky",
+                        age: '3',
+                        favoriteToy: 'Squeaky bone',
+                    },
+                ],
+            },
+            Tobias: {
+                age: '32',
+                location: 'Drammen',
+                password: 'tobiaspass',
+                img: 'imgurl',
+                pets: [
+                    {
+                        img: 'imgurl',
+                        type: 'Ragdoll', //endre
+                        name: "Leo",
+                        age: '2',
+                        favoriteToy: 'Feather teaser',
                     },
                 ],
             },
