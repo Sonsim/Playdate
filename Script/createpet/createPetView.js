d7329cefcd30fb4bdea981e8081d500b3632e59c
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2023-10-24 15:05:21 +0200

    Lagre brukere

---
+++ Script/createpet/createPetView.js
@@ -0,0 +1,34 @@
+function updateViewCreatePet() {
+    let page = document.getElementById('app');
+    let path = model.app.allPages;
+
+    page.innerHTML = /*HTML*/ `
+    <div id=create-pet-background>
+    <div class="create-pet-frame">
+        <img class="pet-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNcvmtYx9E-2KYQ0xX8JN9JN4J8_n3FqbXsA&usqp=CAU"
+            alt=""><br>
+        <button class="upload-image-btn">Legg til bilde</button><br>
+        <input class="pet-input type-of-pet" type="text" placeholder="Type dyr">
+        <hr>
+        <input class="pet-input race" type="text" placeholder="Type rase">
+        <hr>
+        <input class="pet-input name" type="text" placeholder="Navn">
+        <hr>
+        <input class="pet-input age" type="number" placeholder="Alder">
+        <hr>
+
+        <div class="create-pet-bottom-box">
+            <div id="petsAdded">Eks: easy added</div>
+            <div class="create-pet-btn-box">
+                <button class="pet-button" style="margin-right: 2.5px;">Legg til flere dyr</button>
+                <button class="pet-button" onclick="addPetToUser(); updateView('${path.mainPage}')">Opprett bruker</button>
+            </div>
+            <img class="pet-logo" src="img/Playdate.png" alt="">
+        </div>
+
+    </div>
+    </div>
+    
+    ` 
+
+}
