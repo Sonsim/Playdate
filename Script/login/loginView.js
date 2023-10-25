9d6c392bcccfcc615998e7f1de26858cf9b61b7e
kathervlyn <a.h.arnesen@hotmail.com>
2023-10-25 10:38:12 +0200

    Fikset Login CSS

--- Script/login/loginView.js
+++ Script/login/loginView.js
@@ -5,31 +5,32 @@ function updateViewLogin() {
     page.innerHTML = /*HTML*/ `
     <div id="loginBody">
         <div id="loginPage">
-        <h1 id="heading">Playdate</h1>
-        <img src="img/Playdate.png" alt="bilde av hund">
-        <input type="text" placeholder="epost/brukernavn" id="inputDiv">
-        <input type="text" placeholder="passord" id="inputDiv">
-        <div id="buttons">
-        
-            <button id="button"
-             onclick="updateView(
+                <h1 id="heading">Playdate</h1>
+                <img src="img/Playdate.png" alt="bilde av hund">
+                <input type="text" placeholder="epost/brukernavn" id="inputDiv">
+                <input type="text" placeholder="passord" id="inputDiv">
+
+            <div id="buttons">
+                <button class="loginButtons"
+                onclick="updateView(
                 '${path.mainPage}')
                 ">Logg inn</button>
 
-            <button id="button"
-             onclick="updateView(
+                <button class="loginButtons"
+                onclick="updateView(
                 '${path.createUserPage}')
-                ">Ny bruker?</button>
+                ">Ny bruker?</button><br>
 
-            <button id="button"
-            onclick="updateView(
+                <button class="loginButtonForgotPass"
+                onclick="updateView(
                 '${path.forgottenPasswordPage}')
                 ">Glemt passord?</button>
-        </div>
-        <div>
-            <label for="">Husk meg</label>
-            <input type="checkbox" id="myCheckbox" placeholder="Forbli innlogget">
-        </div>
+            </div>
+
+            <div>
+                <label for="">Husk meg</label>
+                <input type="checkbox" id="myCheckbox" placeholder="Forbli innlogget">
+            </div>
         </div>
     </div>
     `
