37ae34eaee54032318c8f6375ded290a3f67773f
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2023-10-25 14:49:48 +0200

    Litt css styling

---
+++ Script/login/loginView.js
@@ -0,0 +1,29 @@
+function updateViewLogin() {
+    let page = document.getElementById('app');
+    let path = model.app.allPages;
+
+    page.innerHTML = /*HTML*/ `
+    <div id="loginBody">
+        <div id="loginPage">
+                <h1 id="heading">Playdate</h1>
+                <img class="logo" src="img/Playdate.png" alt="bilde av hund">
+                <input type="text" placeholder="Epost" id="inputDiv">
+                <input type="text" placeholder="Passord" id="inputDiv">
+            <div id="buttons">
+                <div class="loginButtonsBox">
+                    <button class="loginBtn"onclick="updateView('${path.mainPage}')">Logg inn</button>
+                    <button class="loginButtonForgotPass"onclick="updateView('${path.forgottenPasswordPage}')">Glemt passord?</button>
+                </div>
+                <div class="forgot-password-box">
+                  <button class="newUserBtn"onclick="updateView('${path.createUserPage}')">Ny bruker?</button>
+                </div>
+            </div>
+
+            <div class="remember-me-box">
+                <label for="">Husk meg</label>
+                <input type="checkbox" id="myCheckbox" placeholder="Forbli innlogget">
+            </div>
+        </div>
+    </div>
+    `
+}
