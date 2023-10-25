292ff36546f5d920c11cea05a3e5adf967f7b7f7
kathervlyn <a.h.arnesen@hotmail.com>
2023-10-25 10:17:11 +0200

    Fikset CSS på feedback verktøyet.

--- Script/feedback/feedbackView.js
+++ Script/feedback/feedbackView.js
@@ -3,24 +3,22 @@ function updateFeedbackView() {
   let path = model.app.allPages;
 
   app.innerHTML = /*HTML*/ `
-    <div class="feedbackBody" style="margin: 0;box-sizing: border-box;display: flex;height: 100vh;justify-content: center;">
-      <div class="box" style="{width: 500px;}">
-        <header class="feedback-header style="margin: 40px 0 0 25px;>
-          <img class="logo" style="width: 60px;height: 60px;" src="img/Playdate.png" alt="Logo" />
+    <div class="feedbackBody" >
+      <div class="box">
+        <header class="feedback-header">
+          <img class="logo" src="img/Playdate.png" alt="Logo" />
         </header>
-        <main class="feedback-main" style="display: flex;flex-direction: column;align-items: center;">
-          <input style="width: 200px;margin-bottom: 10px;padding: 2.5px 0 2.5px 0;" type="text" placeholder="Navn" />
-          <input style="width: 200px;margin-bottom: 10px;padding: 2.5px 0 2.5px 0;" type="email" placeholder="Epost" />
-          <input style="width: 200px;margin-bottom: 10px;padding: 2.5px 0 2.5px 0;" type="text" placeholder="Emne" />
-          <textarea style="margin-top: 20px;width: 300px;margin-bottom: 20px;" rows="14" placeholder="Gi en tilbakemelding"></textarea>
-          <button style="width: 120px;border: none;padding: 12px 0 12px 0;border-radius: 7px;cursor: pointer;" onclick="updateView('${path.mainPage}')">Send inn</button>
+        <main class="feedback-main">
+          <input class="feedbackInput" type="text" placeholder="Navn" />
+          <input class="feedbackInput" type="email" placeholder="Epost" />
+          <input class="feedbackInput" type="text" placeholder="Emne" />
+          <textarea class="feedbackTextBox" rows="14" placeholder="Gi en tilbakemelding"></textarea>
+          <button class="feedback-main button" onclick="updateView('${path.mainPage}')">Send inn</button>
         </main>
-        <footer class="feedback-footer" style="margin-left: 25px;">
-          <Button class="back-btn" style="text-decoration: none;color: black;" onclick="updateView('${path.mainPage}')"><i class="fa fa-arrow-left" ></i > Tilbake </Button>
+        <footer class="feedback-footer">
+          <Button class="feedbackBackBtn" onclick="updateView('${path.mainPage}')"><i class="arrow left" ></i > Tilbake </Button>
         </footer>
       </div>
     </div>
     `
-
-
 } 
