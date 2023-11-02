2f65c9584e094c8dbc7e0fc86c1844ce242504f2
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2023-11-02 15:11:49 +0100

    Feilmeldinger og ikke gå videre til neste side uten å skrive inn alt rikitg

---
+++ Script/createuser/createUserView.js
@@ -0,0 +1,39 @@
+function updateViewCreateUser() {
+  let page = document.getElementById("app");
+  const path = model.app.allPages;
+
+  page.innerHTML = /*HTML*/ `
+    <div id="createUserBody">
+        <div id="backbox">
+            <div class="Topline"><h2>Opprett bruker</h2> </div>
+                <img src="img/userprofile/profilepic.png" id="profilepic" alt="Profilbilde placeholder">
+                <hr>
+                <div><span>Last opp bilde: </span><input onchange="uploadImage(this)" type="file" /></div>
+                <div id="wrongImgSrc" class="wrongInput"></div>
+                <hr>
+            <div id="userData">
+                    <input id="nameInput"type="text" placeholder="Fullt navn">
+                    <div id="wrongUserName" class="wrongInput"></div>
+                     <hr>
+                    <input id="birthdayInput" type="text" placeholder="DD/MM/YYYY">
+                    <div id="wrongBirthday" class="wrongInput"></div>
+                     <hr>
+                    <input id="locationInput" type="text" placeholder="Boststed">
+                    <div id="wrongLocation" class="wrongInput"></div>
+                    <hr>
+                    <input id="emailInput"type="text" placeholder="Epostadresse">
+                    <div id="wrongEmail" class="wrongInput"></div>
+                    <hr>
+                    <input id="passwordInput" type="password" placeholder="Passord">
+                    <div id="wrongPassword" class="wrongInput"></div>
+                    <hr>
+                    <input id="reapeatPasswordInput" type="password" placeholder="Gjenta passord">
+                    <div id="wrongRepeatPassword" class="wrongInput"></div>
+                    <hr>
+            </div>
+                <button onclick="createNewUser()">Videre</button> <br>
+                <div><img id="logo" src="img/Playdate.png" alt="Playdate Logo"> </div><br>
+                <button id="backbutton" onclick="updateView('${path.loginPage}')">Tilbake</button>
+        </div>
+    </div>`;
+}
