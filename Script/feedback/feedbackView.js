63b50799249368cacb52eb4513f5bb7427508b42
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2023-11-07 23:31:16 +0100

    Litt styling

--- Script/feedback/feedbackView.js
+++ Script/feedback/feedbackView.js
@@ -7,17 +7,18 @@ function updateFeedbackView() {
       <div class="box">
         <header class="feedback-header">
           <img class="logo" src="img/Playdate.png" alt="Logo" />
+          <h2>Gi oss en tilbakemelding</h2>
         </header>
         <main class="feedback-main">
           <input id="nameInput" class="feedbackInput" type="text" placeholder="Navn" />
           <input id="emailInput" class="feedbackInput" type="email" placeholder="Epost" />
           <input id="subjectInput" class="feedbackInput" type="text" placeholder="Emne" />
           <textarea id="contentInput" class="feedbackTextBox" rows="14" placeholder="Gi en tilbakemelding"></textarea>
-          <button class="feedback-main button" onclick="sendFeedback(); updateView('${path.mainPage}')">Send inn</button>
+          <div class="button-box">
+            <button class="feedbackBackBtn" onclick="updateView('${path.mainPage}')"><i class="arrow left"></i>Tilbake</button>
+            <button class="feedback-main-button" onclick="sendFeedback(); updateView('${path.mainPage}')">Send inn</button>
+          </div>
         </main>
-        <footer class="feedback-footer">
-          <Button class="feedbackBackBtn" onclick="updateView('${path.mainPage}')"><i class="arrow left" ></i > Tilbake </Button>
-        </footer>
       </div>
     </div>
     `
